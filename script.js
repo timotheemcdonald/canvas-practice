2c80af7e119f09fae228ed4c193a2db271b955b3
timotheemcdonald <timotheemcdonald@gmail.com>
2021-01-18 22:25:30 -0500

    added fish file

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const numberOfParticles = 200;
+let particlesArray = [];
 
+class Particle {
+    
+}
